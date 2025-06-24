01214e17d7e6c03928a2eb127fae100589ded475
iceice1005 <comflyuisdxlver1@gmail.com>
2025-06-24 08:07:36 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,11 @@ import { defineConfig, loadEnv } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
+    const env = loadEnv(mode, process.cwd(), '');
     
     return {
         define: {
             'process.env.API_KEY': JSON.stringify(env.API_KEY),
-            'process.env.GEMINI_API_KEY': JSON.stringify(env.API_KEY)
         },
         resolve: {
             alias: {
